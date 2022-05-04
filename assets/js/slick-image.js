743e4cb4722f71d9103a3cc90f5b1c2827cb76f0
Ketaki <ket.ck23@rediffmail.com>
2022-05-04 22:30:32 +0530

    Auto-generated commit [ci skip]

---
+++ assets/js/slick-image.js
@@ -0,0 +1,13 @@
+     function slide(){
+        $('.service_slide_item').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000
+
+      })
+
+
+
+     }
+    
